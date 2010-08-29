ddb98defbb2bccec72cb118390baf693fb7fe243
Doktor Bro <Doktor Bro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-29 22:07:47 +0000

    Delete temporary file. Change description.
    git-svn-id: https://plugins.svn.wordpress.org/readable-names/tags/0.3.1@282378 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readable-names.php
+++ readable-names.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Readable Names
 Plugin URI: http://wordpress.org/extend/plugins/readable-names/
-Description: The Readable Names plugin forces users to write their names in the language that your blog uses.
-Version: 0.2.1
+Description: The plugin forces users to write their names in the language that your blog uses.
+Version: 0.3.1
 Author: Anatol Broder
 Author URI: http://doktorbro.net/
 License: GPL2
