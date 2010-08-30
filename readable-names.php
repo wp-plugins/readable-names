93991c5ba58a93154994e31c82d5dd6c407fc26b
Doktor Bro <Doktor Bro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-30 00:52:55 +0000

    Add text domain to error messages
    git-svn-id: https://plugins.svn.wordpress.org/readable-names/tags/0.4.2@282408 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readable-names.php
+++ readable-names.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: Readable Names
 Plugin URI: http://wordpress.org/extend/plugins/readable-names/
-Description: The Readable Names plugin forces users to write their names in the language that your blog uses.
-Version: 0.2.1
+Description: The plugin forces commenters to write their names in the language that your blog uses.
+Version: 0.4.2
 Author: Anatol Broder
 Author URI: http://doktorbro.net/
 License: GPL2
-Text Domain: readable-names
+Text Domain: readable_names
 */
 
 if ( ! function_exists( 'is_admin' ) ) {
@@ -83,7 +83,7 @@ class Readable_Names {
 		
 		$result = $this->check_full_name($comment_author);
 		if ( $result )
-			wp_die( $result, __( 'Error: non readable name' ) . ' | ' . get_bloginfo ( 'name' ), 
+			wp_die( $result, __( 'Error: non readable name', plugin_identifier ) . ' | ' . get_bloginfo ( 'name' ), 
 				array( 'response' => 500, 'back_link' => true ) );
 	}
 
@@ -134,7 +134,7 @@ class Readable_Names {
 								$this->options_field( 'allowed_capital_letters' ) .
 								$this->options_field( 'allowed_digits' );
 		if ( $this->strings_compare_count( $allowed_characters, $input ) < $length ) {
-			$result = sprintf( __( 'Name &ldquo;%s&rdquo; contains invalid characters.' ), $name );
+			$result = sprintf( __( 'Name &ldquo;%s&rdquo; contains invalid characters.', plugin_identifier ), $name );
 		}
 
 		return $result;
@@ -152,7 +152,7 @@ class Readable_Names {
 		$result = null;
 		
 		if ( $this->strings_compare_count( 0 == $this->options_field( 'required_letters' ), $name ) ) {
-			$result = sprintf( __( 'Name &ldquo;%s&rdquo; does not contain required letters &ldquo;%s&rdquo;.' ), $name, $this->options_field( 'required_letters' ) );
+			$result = sprintf( __( 'Name &ldquo;%s&rdquo; does not contain required letters &ldquo;%s&rdquo;.', plugin_identifier ), $name, $this->options_field( 'required_letters' ) );
 		}
 
 		return $result;
@@ -170,7 +170,7 @@ class Readable_Names {
 		$length = mb_strlen( $name );
 
 		if ( $length <  $this->options_field( 'minimum_name_length' ) ) {
-			$result = sprintf( __( 'Name &ldquo;%s&rdquo; is too short. It has to be at least %d characters long.' ), $name, $this->options_field( 'minimum_name_length' ) );
+			$result = sprintf( __( 'Name &ldquo;%s&rdquo; is too short. It has to be at least %d characters long.', plugin_identifier ), $name, $this->options_field( 'minimum_name_length' ) );
 		}
 
 		return $result;
@@ -200,7 +200,7 @@ class Readable_Names {
 
 		$first_letter = mb_substr( $name, 0, 1 );
 		if ( $this->strings_compare_count( $first_letter, $this->options_field( 'allowed_capital_letters' ) ) == 0 ) {
-			$result = sprintf( __( 'Name &ldquo;%s&rdquo; does not begin with a capital letter.' ), $name );
+			$result = sprintf( __( 'Name &ldquo;%s&rdquo; does not begin with a capital letter.', plugin_identifier ), $name );
 		}
 		
 		return $result;
@@ -218,7 +218,7 @@ class Readable_Names {
 		$result = null;
 		
 		if ( $this->strings_compare_count( $name, $this->options_field( 'allowed_capital_letters' ) ) > 1 ) {
-			$result = sprintf( __( 'Name &ldquo;%s&rdquo; has to many capital letters.' ), $name );
+			$result = sprintf( __( 'Name &ldquo;%s&rdquo; has to many capital letters.', plugin_identifier ), $name );
 		}
 		
 		return $result;
--- readme.txt
+++ readme.txt
@@ -3,13 +3,13 @@ Contributors: Doktor Bro
 Tags: comments, usability, etiquette
 Requires at least: 3.0
 Tested up to: 3.0.1
-Stable tag: 0.2.1
+Stable tag: 0.4.2
 
-The Readable Names plugin forces users to write their names in the language that your blog uses.
+The plugin forces commenters to write their names in the language that your blog uses.
 
 == Description ==
 
-The Readable Names plugin forces users to write their names in the language that your blog uses. Using names written in your blog's native language will enhance the quality of users' conversation. Customizing predefined language standards can create your own slang. Less spam is a welcome side effect.
+The plugin forces commenters to write their names in the language that your blog uses. Using names written in your blog's native language will enhance the quality of users' conversation. Customizing predefined language standards can create your own slang. Less spam is a welcome side effect.
 
 = Predefined languages =
 * English
@@ -31,6 +31,26 @@ or download through the 'Plugins' menu in WordPress.
 2. The error messages are user friendly.
 
 == Changelog ==
+
+= 0.4.2 =
+* Add text domain to error messages
+
+= 0.4.1 =
+* Typo in german and russian translation
+
+= 0.4 =
+* Add german translation
+* Change the plugin description
+
+= 0.3.2 =
+* Wrong text domain
+
+= 0.3.1 =
+* Delete temporary file
+* Change description
+
+= 0.3 =
+* Add russian translation
 
 = 0.2 =
 * Add screenshots
